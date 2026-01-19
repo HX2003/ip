271177660d37d05ceaa36b66a7781dea5a13eb81
HX2003 <HX2003@users.noreply.github.com>
2026-01-19 22:05:03 +0800

    Rename chatbot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Quark User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Quark.java
@@ -0,0 +1,11 @@
+public class Quark {
+    public static void main(String[] args) {
+        String logo = "Hello! I'm Quark\n"
+        + "What can I do for you?\n"
+        + "____________________________________________________________\n"
+        + "Bye. Hope to see you again soon!\n"
+        + "____________________________________________________________\n";
+
+        System.out.println(logo);
+    }
+}
