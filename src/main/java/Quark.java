09fd6bb4cbf5769f0e5db599b362e3f71ef0caf7
HX2003 <HX2003@users.noreply.github.com>
2026-01-24 22:58:31 +0800

    Add list command
    Non-commands are now stored in a list, which can be retrieved using the list command.

--- src/main/java/Quark.java
+++ src/main/java/Quark.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Quark {
     private static final String SEPARATOR = "____________________________________________________________";
 
     public static void main(String[] args) {
+        List<Task> tasks = new ArrayList<>();
+
         Scanner in = new Scanner(System.in);
 
         String logo = "Hello! I'm Quark" + System.lineSeparator()
@@ -19,8 +23,17 @@ public class Quark {
                 break;
             }
 
+            if (line.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                  System.out.println((i + 1) + ". " + tasks.get(i).getDescription());
+                }
+                continue;
+            }
+
+            tasks.add(new Task(line));
+
             String reply = SEPARATOR + System.lineSeparator()
-            + "▶ " + line + System.lineSeparator()
+            + "▶ added: " + line + System.lineSeparator()
             + SEPARATOR;
             
             System.out.println(reply);
