dab07c7b881fcaddf8446d7ec01c194c6577f5d2
HX2003 <HX2003@users.noreply.github.com>
2026-01-24 22:42:56 +0800

    Add bye command, and echo commands

--- src/main/java/Quark.java
+++ src/main/java/Quark.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class Quark {
+    private static final String SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = "Hello! I'm Quark\n"
-        + "What can I do for you?\n"
-        + "____________________________________________________________\n"
-        + "Bye. Hope to see you again soon!\n"
-        + "____________________________________________________________\n";
+        Scanner in = new Scanner(System.in);
+
+        String logo = "Hello! I'm Quark" + System.lineSeparator()
+        + "What can I do for you?" + System.lineSeparator()
+        + SEPARATOR;
 
         System.out.println(logo);
+
+        while (true) {
+            String line = in.nextLine();
+
+            if (line.equals("bye")) {
+                break;
+            }
+
+            String reply = SEPARATOR + System.lineSeparator()
+            + "▶ " + line + System.lineSeparator()
+            + SEPARATOR;
+            
+            System.out.println(reply);
+        }
+
+        String byeReply = SEPARATOR + System.lineSeparator()
+        + "▶ Bye. Hope to see you again soon!" + System.lineSeparator()
+        + SEPARATOR;
+
+        System.out.println(byeReply);
     }
 }
