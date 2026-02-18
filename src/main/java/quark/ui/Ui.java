b824d263d4e75f49d69c228df02ea5d70e961052
HX2003 <HX2003@users.noreply.github.com>
2026-02-19 05:18:03 +0800

    Support multiple arguments for printReply
    Utilize Java varargs for cleaner code

--- src/main/java/quark/ui/Ui.java
+++ src/main/java/quark/ui/Ui.java
@@ -17,20 +17,22 @@ public class Ui {
         return "";
     }
 
-    public static void printReply(String reply) {
-        System.out.println(SEPARATOR + System.lineSeparator()
-                + reply + System.lineSeparator()
-                + SEPARATOR);
+    public static void printReply(String... reply) {
+        System.out.println(SEPARATOR);
+        for (String r : reply) {
+            System.out.println(r);
+        }
+        System.out.println(SEPARATOR);
     }
 
     public static void printHelloReply(boolean isLoadedFromFile, String path) {
         if (isLoadedFromFile) {
-            printReply("Hello again! I'm Quark" + System.lineSeparator()
-                    + "I have loaded your tasks from " + path + System.lineSeparator()
-                    + "what can I do for you?");
+            printReply("Hello again! I'm Quark",
+                    "I have loaded your tasks from " + path,
+                    "what can I do for you?");
         } else {
-            printReply("Hello! I'm Quark" + System.lineSeparator()
-                    + "Looks like you're new here, what can I do for you?");
+            printReply("Hello! I'm Quark",
+                    "Looks like you're new here, what can I do for you?");
         }
     }
 
