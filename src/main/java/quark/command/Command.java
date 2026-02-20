e2fbe7acdabe14b4ae679f84b07e5c30c05b3a46
HX2003 <HX2003@users.noreply.github.com>
2026-02-20 20:14:15 +0800

    Add find command

--- src/main/java/quark/command/Command.java
+++ src/main/java/quark/command/Command.java
@@ -5,6 +5,7 @@ public class Command {
     public static final String BYE = "bye";
     public static final String ANNIHILATE = "annihilate";
     public static final String LIST = "list";
+    public static final String FIND = "find";
     public static final String TODO = "todo";
     public static final String DEADLINE = "deadline";
     public static final String EVENT = "event";
